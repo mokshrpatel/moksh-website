4bb713f2eba3d1eb1d7aae9627232e9820cc945a
mokshrpatel <mokshrpatel2005@gmail.com>
2025-02-14 07:52:45 +0530

    Add files via upload
    Dynamic data displaying added

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
+from flask import render_template
+#reder_template is a function that takes a template and a dictionary and renders the template with the dictionary
+
 
 app = Flask(__name__)
 
-
+companies = ['Microsoft', 'Google', 'Paypal', 'Meta']
+jobs = [
+    {
+        'id' : 1, 'company': 'Microsoft', 'salary': 12000
+    },
+    {
+        'id' : 2, 'company': 'Google', 'salary': 13000
+    },
+        {
+        'id' : 3, 'company': 'Meta', 'salary': 14000
+    },
+    {
+        'id' : 4, 'company': 'Paypal', 'salary': 15000
+    },
+]
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template("home.html", name = "Moksh R. Patel", companies = companies, jobs=jobs )
 
 
 if __name__ == '__main__':
