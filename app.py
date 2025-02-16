2e2337740d4990ff31539df7f1ad016b42b5069d
mokshrpatel <mokshrpatel2005@gmail.com>
2025-02-16 11:54:15 +0530

    Added database

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from flask import render_template
-#reder_template is a function that takes a template and a dictionary and renders the template with the dictionary
+from flask import render_template #reder_template is a function that takes a template and a dictionary and renders the template with the dictionary
+
+from database import load_jobs_from_db  #if we importing something form the antoher python file then that file will be running first.
+
 
 
 app = Flask(__name__)
@@ -19,9 +21,12 @@ jobs = [
     {
         'id' : 4, 'company': 'Paypal', 'salary': 15000
     },
-]
+]        
+
+
 @app.route("/")
 def hello():
+    jobs = load_jobs_from_db()
     return render_template("home.html", name = "Moksh R. Patel", companies = companies, jobs=jobs )
 
 
---
+++ database.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine, text
+
+# syntax for connecting sqlalchemy to mysql
+# engine = create_engine( "mysql+pymysql://user:pass@host/dbname?charset=utf8mb4")
+db_connection_string = ${{ secrets.DB_CONNECTION_STRING }}
+engine = create_engine(db_connection_string)
+
+# more secure way to connect with SSL
+
+
+
+# with engine.connect() as conn:
+#     result = conn.execute(text("select * from jobs"))
+#     print(type(result))
+#     # here result is like a file stream, means if we go through it one time then we can not read it again.
+#     printResult = result.all()
+#     print(printResult)
+#     print("type of returning object from all():", type(printResult))
+#     print("type of every index of that list:", type(printResult[0])) 
+
+#     for _ in range(50):  print(end="-")
+#     print()
+
+#     # now we convert each row into dictionary type because we don't know how to deal with that type
+#     # so now we will make list of dictionary
+
+    
+#     result_all = [dict(row._mapping) for row in printResult]
+#     # for row in printResult:
+#     #     result_all.append(dict(row))
+
+#     print(result_all)
+
+# # print(result_all)
+
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = [dict(row._mapping) for row in result.all()]
+        return jobs
